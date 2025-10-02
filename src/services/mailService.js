0369d6a28573d6312ac0b74996c23c59a538022d
Gaurav singh <gauravbora2273@gmail.com>
2025-10-02 11:46:33 +0530

    adding the secure: true

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -4,6 +4,7 @@ export const sendVerificationEmail = async (email, otp) => {
   // transporter (using Gmail here, but you can use any SMTP service)
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        secure: true,
         auth: {
         user: process.env.EMAIL_USER,  
         pass: process.env.EMAIL_PASS   
