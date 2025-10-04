5a22c131f929df94eef03b8404cf0b310b3a9fe5
Gaurav singh <gauravbora2273@gmail.com>
2025-10-04 13:46:29 +0530

    add post management routes and implement fetching and deleting posts

--- src/controllers/post/post.controllers.js
+++ src/controllers/post/post.controllers.js
@@ -46,3 +46,46 @@ export const createPost = asyncHandler(async (req, res) => {
     }
     res.status(201).json(new ApiResponse(201, 'Post created successfully', createdPost));
 });
+
+// get all posts
+export const getAllPosts = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    if(!userId){
+        throw new ApiError(401, 'Unauthorized');
+    }
+    const posts = await Post.find().populate('userId', 'username email').sort({createdAt: -1});
+    if(!posts){
+        throw new ApiError(500, 'Something went wrong' );
+    }
+    res.status(200).json(new ApiResponse(200, 'Posts fetched successfully', posts));
+});
+
+// get post by userid
+export const getPostByUserId = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    if(!userId){
+        throw new ApiError(401, 'Unauthorized');
+    }
+    const posts = await Post.find({userId}).populate('userId', 'username email').sort({createdAt: -1});
+    if(!posts){
+        throw new ApiError(404, 'No posts found for this user');
+    }
+    res.status(200).json(new ApiResponse(200, 'Posts fetched successfully', posts));
+});
+
+// delete post by postid
+export const deletePostById = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    const postId = req.params.id;   
+    if(!userId){
+        throw new ApiError(401, 'Unauthorized');
+    }
+    if(!postId){
+        throw new ApiError(400, 'Post ID is required');
+    }
+    const post = await Post.findOneAndDelete({_id: postId, userId});
+    if(!post){
+        throw new ApiError(404, 'Post not found or you are not authorized to delete this post');
+    }
+    res.status(200).json(new ApiResponse(200, 'Post deleted successfully'));
+});
--- src/routes/post/post.routes.js
+++ src/routes/post/post.routes.js
@@ -1,15 +1,18 @@
 import {Router} from 'express';
-import { createPost } from '../../controllers/post/post.controllers.js';
+import { createPost,getAllPosts,getPostByUserId,deletePostById } from '../../controllers/post/post.controllers.js';
 import {verifyJWT} from '../../middlewares/auth.middleware.js';
 import { upload } from '../../middlewares/multer.middleware.js';
 
-
 const router = Router();
+
 router.route('/create-post').post(
     verifyJWT,
     upload.fields([
         { name: 'content', maxCount: 5 },
     ]),
     createPost);
+router.route('/get-all-posts').get(verifyJWT, getAllPosts);
+router.route('/get-post-userId').get(verifyJWT, getPostByUserId);
+router.route('/delete-post/:id').delete(verifyJWT,deletePostById);
 
 export default router;
